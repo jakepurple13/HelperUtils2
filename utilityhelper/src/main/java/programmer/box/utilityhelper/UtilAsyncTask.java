f1a87dc02f1eaf051f18be2a8a2ee2526559caa9
Jacob <jakerein@gmail.com>
2017-10-10 11:38:48 -0400

    -added utility asynctask -added permissions

---
+++ utilityhelper/src/main/java/programmer/box/utilityhelper/UtilAsyncTask.java
@@ -0,0 +1,51 @@
+package programmer.box.utilityhelper;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Jacob on 10/10/17.
+ */
+
+public abstract class UtilAsyncTask extends AsyncTask<Void, Integer, Boolean> {
+
+    abstract void onPreExecutes();
+
+    abstract boolean onBackgrounds(Void... voids);
+
+    abstract void onPostExecutes(Boolean o);
+
+    abstract void onProgressUpdates(Integer[] values);
+
+    public void updateProgress(Integer... values) {
+        publishProgress(values);
+    }
+
+    @Override
+    protected void onPostExecute(Boolean o) {
+        super.onPostExecute(o);
+        onPostExecutes(o);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        onPreExecutes();
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer[] values) {
+        super.onProgressUpdate(values);
+        onProgressUpdates(values);
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... voids) {
+        return onBackgrounds(voids);
+    }
+
+    public UtilAsyncTask startTask(Void...params) {
+        execute(params);
+        return this;
+    }
+
+}
--- utilityhelper/src/main/java/programmer/box/utilityhelper/UtilDevice.java
+++ utilityhelper/src/main/java/programmer/box/utilityhelper/UtilDevice.java
@@ -1,12 +1,21 @@
 package programmer.box.utilityhelper;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.view.Window;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jacob on 10/5/17.
@@ -50,6 +59,33 @@ public class UtilDevice {
 
         public void currentLevel(int level);
 
+    }
+
+    /**
+     * requestPermissions - request permissions
+     * @param activity  Activity
+     * @param permissionsList  list of permissions
+     *                         should look something like: +
+     *                         new String[]{@link Manifest.permission}.CAMERA, {@link Manifest.permission}.VIBRATE};
+     *                         + Don't forget to add permissions to the manifest.xml "permission android:name="android.permission.CAMERA"
+     * @param requestCode  request code for activity result
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    public static void requestPermissions(Activity activity, String[] permissionsList, int requestCode) {
+
+        List<String> permissions = new ArrayList<>();
+
+        // loop through permissions
+        for (String permission : permissionsList) {
+            if (activity.checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(permission);
+            }
+        }
+
+        // if permissions list is not empty, request permission
+        if (!permissions.isEmpty()) {
+            activity.requestPermissions(permissions.toArray(new String[permissions.size()]), requestCode);
+        }
     }
 
 }
